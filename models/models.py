52be07d251aa5c398bd094847ca45d5a13183394
Owenmur21 <owenmuriithi@gmail.com>
2020-02-07 15:08:20 +0300

    Corrects menu

--- models/models.py
+++ models/models.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api
 
 class Book(models.Model):
-    _name="library.book"
+    _name='library.book'
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
