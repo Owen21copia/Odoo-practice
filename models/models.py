47f933aa4bdba457bfb5f049cf67ffe77828ebd7
Owenmur21 <owenmuriithi@gmail.com>
2020-02-07 12:40:17 +0300

    Creates Book model

--- models/models.py
+++ models/models.py
@@ -2,14 +2,8 @@
 
 from odoo import models, fields, api
 
-# class library(models.Model):
-#     _name = 'library.library'
+class Book(models.Model):
+    _name="library.book"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
