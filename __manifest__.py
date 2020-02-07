8d30fa1f42be6c60beea40480d7275890ef9283d
Owenmur21 <owenmuriithi@gmail.com>
2020-02-07 14:36:17 +0300

    Defines Menu entries

--- __manifest__.py
+++ __manifest__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/library.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
