3496950519b5737920786944140e61beca690277
Owenmur21 <owenmuriithi@gmail.com>
2020-02-07 16:07:12 +0300

    Creates shelf class

--- models/models.py
+++ models/models.py
@@ -3,7 +3,13 @@
 from odoo import models, fields, api
 
 class Book(models.Model):
-    _name='library.book'
+    _name = 'library.book'
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+
+class Shelf(models.Model):
+    _name = 'library.shelf'
+
+    name = fields.Char(string="Title", required=True)
+    spaces = fields.Integer(string="Number of spaces")
