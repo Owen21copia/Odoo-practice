1041f00efeaa26c004baa0a52bbf19fd83ff5e5f
Owenmur21 <owenmuriithi@gmail.com>
2020-02-12 09:56:09 +0300

    Adds field for computing remaining spaces in the shelf

--- models/models.py
+++ models/models.py
@@ -18,6 +18,19 @@ class Shelf(models.Model):
     spaces = fields.Integer(string="Number of spaces")
     manager_id = fields.Many2one('res.partner', string="Manager")
     book_ids = fields.One2many('library.book', 'shelf_ids', string="Book")
+    remaining_spaces = fields.Integer(string="Remaining spaces", compute = 'rem_spaces')
+
+    @api.depends('spaces' , 'book_ids')
+    def rem_spaces(self):
+        for r in self:
+            if not r.seats:
+                r.remaining_spaces = 0
+            else:
+                r.remaining_spaces = (spaces - len(book_ids))
+
+
+
+    
     
 
     
